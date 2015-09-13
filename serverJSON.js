c5e4e2b667c14feca03c7d83109e9c53a9453bdf
Brett Lyons <blyons@students.naropa.edu>
2015-09-13 12:17:43 -0600

    Added serverJSON.js with boilerplate (NOT YET WORKING)

--- serverJSON.js
+++ serverJSON.js
@@ -12,7 +12,14 @@ var http = require('http');
 var url = require('url');
 
 var server = http.createServer(function (req, res) {
-  if(req.method == 'GET'){ }
+  if(req.method == 'GET'){
+  res.writeHead(200, {'Content-Type':'application/json'});
+    if (urlKeys.pathname == '/api/gameState') {
+      var tmpDate = new Date(urlKeys.query.iso);
+      jsonResponse.unixtime = tmpDate.getTime();
+      res.end(JSON.stringify(jsonResponse));
+    }
+  }
   if (req.method == 'POST') { }
   res.writeHead(200, {'Content-Type':'application/json'});
   var urlKeys = url.parse(req.url, true);
@@ -22,14 +29,9 @@ var server = http.createServer(function (req, res) {
   // and also to respond differently to /api/chooseMove
   var jsonResponse = {};
 
-  if (urlKeys.pathname == '/api/chooseMove') {
-    jsonResponse.yourMove =
+  if (urlKeys.pathname == '/api/chooseMove/:move') {
+    jsonResponse.yourMove = move;
     //console.log(jsonResponse);
-    res.end(JSON.stringify(jsonResponse));
-  }
-  if (urlKeys.pathname == '/api/unixtime') {
-    var tmpDate = new Date(urlKeys.query.iso);
-    jsonResponse.unixtime = tmpDate.getTime();
     res.end(JSON.stringify(jsonResponse));
   }
   else {
